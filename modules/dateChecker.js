e900fd2731950f9973d57eadaee891cc4e975dc4
System <none>
2017-08-08 18:52:27 +0000

    working implementation

--- modules/dateChecker.js
+++ modules/dateChecker.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const app = express();
 const moment = require('moment');
-const dateChecker = require('./modules/dateChecker.js');
-moment().format();
 
-let output ={ "unix": null, "natural": null };
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html', function(err) {
-    if (err) throw err;
-    console.log(dateChecker(562151515))
-  });
-});
-
-app.get('/:data', function(req, res) {
-  //res.send(req.params.data);
-  let input = req.params.data;
+module.exports = function(input) {
+  let output = { "unix": null, "natural": null };
   if(!(/\D/g.test(input))){
+    output.unix = Number(input);
+    output.natural = moment.unix(input).format('MMMM DD, YYYY');
+    return output;
+  } else {
+    if (moment(input).isValid()){
+      output.unix = Number(moment(input).format('X'));
+      output.natural = moment(input).format('MMMM DD, YYYY');
+      return output;
+    } else {
+      return { "unix": null, "natural": null };
+    }
+  }
+}
+
+/*if(!(/\D/g.test(input))){
     //console.log('proceeding with unix - all digits input')
     let unixDate = input;
     let humanDate = moment.unix(input);
@@ -35,6 +36,4 @@ app.get('/:data', function(req, res) {
       res.send(output);
     }
   }
-});
-
-app.listen(3000);
+});*/
